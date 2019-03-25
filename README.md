0a9861b4cadb73949b305f28e2b2b2f4e9a8f642
Zia <ziadhorat@gmail.com>
2019-03-26 00:48:40 +0200

    80 char max

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ### Note
 ###### ⚫Only works on .c files
-###### ⚫This is extremely messy code, I'm just trying to get basic detection working
+###### ⚫This is extremely messy code, I'm just trying to get basic detection working, everythin is pretty much in one function, I repeat, this is horrible programming practices!
 
 ### Things that work??? (I didn't test)
 ###### ⚫Empty lines (Gives error if there are multiple '\n's next to each other)
@@ -15,6 +15,7 @@
 ###### ⚫Empty line after header (Only checks if header is perfect)
 ###### ⚫New line after pre-proc (Checks for '\n' after the final '#')
 ###### ⚫Too many lines in function (Based on open and close curly braces { })
+###### ⚫80 character per line max
 ###### ⚫Final line can't be empty ('\0' after '\n' & '\n')
 ###### ⚫Trailing spaces (Spaces before ' ' or '\t')
 
@@ -22,7 +23,6 @@
 ###### ⚫All pre-processors at top of file (Technically only makes sure they're all together)
 
 ### Coming soon (I'll do it at some point)
-###### ⚫80 character per line
 ###### ⚫5 functions per file
 ###### ⚫Space after while/if/return etc
 ###### ⚫If theres a curly brace, there can't be anything else on the line
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: zmahomed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 07:28:15 by zmahomed          #+#    #+#             */
-/*   Updated: 2019/03/25 19:00:19 by zmahomed         ###   ########.fr       */
+/*   Updated: 2019/03/26 00:47:07 by zmahomed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		disp_file(char *str)
 	int		line_num = 0;
 	int		func_line = 0;
 	int		func_count = 0;
+	int		char_count  = 0;
 	int		errors = 0;
 	int		pre_proc = 0;
 	char	buf[BUF_SIZE + 1];
@@ -35,9 +36,15 @@ int		disp_file(char *str)
 		buf[ret] = '\0';
 		while (buf[i])
 		{
+			if (char_count > 80)
+			{
+					printf("\033[96;1m%i\033[0m : \033[31m%i\033[0m characters in line\n",line_num, char_count);
+					errors++;
+			}
 			if (buf[i] == '\n')
 			{
 				line_num++;
+				char_count = 0;
 				if (line_num < 11 && buf[i + 1] != '/' && errors == 0)
 				{
 					printf("\033[96;1m%i\033[0m : Incorrect 42Header\n",line_num);
@@ -104,6 +111,8 @@ int		disp_file(char *str)
 					errors++;
 				}
 			}
+			else
+				char_count++;
 			i++;
 		}
 		close(fd);
